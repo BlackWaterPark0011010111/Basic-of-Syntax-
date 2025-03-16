05905cf4f5c867c1433582aafbda99e170ece023
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-03-16 23:51:48 +0100

    dict fixing

--- dictionaries/12.10_dict2.py
+++ dictionaries/12.10_dict2.py
@@ -17,21 +17,25 @@ students = {
 }
 
 print(students['student1'][0][3]) # выводим букву 'a'- последнюю буквк в слове Anna, [3]- 
-# это ее индекс
-print(students['student1'])#чтобы вывксти определенного студента
-#по ключу
-#открываем внетри квадратные скобки и он покажет нам что включает
-# # нам student1
-print(students['student1'][0]) # 0 мы ставим если в Anna выглядел 
+# это ее индекс   we output the letter 'a' - the last letter in the word Anna, [3] - 
+# this is its index
+print(students['student1'])#чтобы вывксти определенного студента по ключу 
+#to extract a specific student by key
+#открываем внетри квадратные скобки и он покажет нам что включает student1
+#  we open the three square brackets and it will show us what student1 includes
+print(students['student1'][0]) # 0 мы ставим если в Anna выглядел 0 we put if in Anna looked
 #бы как лист, например
 'student1' : [ 'Anna','Berlin' ]
 'student1' : [ 'Anna','Berlin', {'k':'v', 'm':'s'} ]
-#ксли нам нужно досткчатся др K V M s до словаря внетри словаряб
-# # мы указываем
-print(students['student1'][0][2]['m']) # 2 значит индекс словар
-
+#ксли нам нужно досткчатся др K V M s до словаря внетри словаряб, мы указываем
+#if we need to access a dictionary outside of three dictionaries, we specify
+print(students['student1'][0][2]['m'])
+# 2 значит индекс словар
+# 2 means dictionary index
 #внетри словаря который равен "2", 
 #и потом указыванм то что там нам нудно найти
+#outside the dictionary which is equal to "2", 
+#and then we indicate what we need to find there
 
 students = {
     'student1' : ['Anna','Berlin',{'key':'value','m':'s'}],
@@ -75,7 +79,7 @@ print(students['drinks']['wine'])
 
 dict2 =dict(['one', 'two'])
 dict2 = dict(list1) # нельзя вот так вставить лист
-
+#you can't insert a sheet like this
 
 #сначала 
 dict = {}
@@ -92,7 +96,7 @@ fruits = {
 
 
 #looping over lists as values
-for key, value in fruits.items():# выводит все values  
+for key, value in fruits.items():# выводит все values   prints all values
     for x in value:
         print(x)
 
@@ -106,7 +110,7 @@ for k, v in fruits.items():
 
 for  value in fruits.items():
     print( value[0])# выводит apple, orange, banana
-
+#outputs apple, orange, banana
 
 
 for  value in fruits.items():
