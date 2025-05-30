9525c0a0b59f3ce982f38fa39f7cbfc58e324a52
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-05-30 23:52:05 +0200

    fix main1.py

--- 1__Control Flow/Conditionals/if_else/or_and_not.py
+++ 1__Control Flow/Conditionals/if_else/or_and_not.py
@@ -6,5 +6,4 @@ if x==10 or age==18:
    print("You are on our platform")
 else:
    print("Sorry, not all conditions were met")
-   #ternary statement
    print("yes, good")    if not(x==10 or age==18) else  print("Sorry, not possible")
