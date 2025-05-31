f4c7df2d304dde5ef76d061c86b769e26e224fd8
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-05-31 23:30:51 +0200

    fix

--- 1__Control Flow/Error Handling/Exceptions/test2.py
+++ 1__Control Flow/Error Handling/Exceptions/test2.py
@@ -48,3 +48,40 @@ except KeyboardInterrupt:
 class Test:
     def method(self):
         raise NotImplementedError("потом доделаю")
+
+try:
+    "строка".qwerty()
+except AttributeError:
+    print("нет такого метода")
+
+#if True print("ой")  
+#def test():
+#print("плохо")  # так нельзя
+
+try:
+    pass
+except (TypeError, ValueError):#ловить несколько ошибок сразу
+
+    print("или тип не тот, или значение")
+
+try:
+    pass
+except Exception as e:
+    print(f"случилось {e}")
+
+try:
+    f = open("file.txt")
+except:
+    print("ошибка")
+finally:#finally выполнится всегда
+
+    print("убираем за собой")
+    # f.close()  #например
+
+try:#else если не было ошибок
+
+    print("всё ок")
+except:
+    print("ошибка")
+else:
+    print("значит ошибок не было")
