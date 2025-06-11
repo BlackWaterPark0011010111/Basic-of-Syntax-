2d73488da93cd50c5bef716cbe571754587b7ad7
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-06-11 22:52:56 +0200

    fixin  main.py whileloop

--- 1__Control Flow/Loops/While_Loops/main.py
+++ 1__Control Flow/Loops/While_Loops/main.py
@@ -2,3 +2,26 @@ count = 0
 while count < 5:
     print(f"Это строка №{count+1}")
     count += 1  
+
+password = ""
+while password != "12345":  
+    password = input("пароль: ")
+print("Добро пожаловать!")
+
+while True:#ошибки в цикле
+    try:
+        num = int(input("Введите число: "))
+        break
+    except ValueError:
+        print("Это не число! Попробуйте ещё раз")
+
+choice = ""
+while choice != "4":
+    print("\n1.  время\n2. дата\n3. Шутка\n4. Выход")
+    choice = input("Выберите пункт: ")
+    if choice == "1":
+        print("Сейчас  день")
+    elif choice == "2":
+        print("сегодня 2025 год")
+    elif choice == "3":
+        print("программист на рыбалке... while True: try_to_catch()")
